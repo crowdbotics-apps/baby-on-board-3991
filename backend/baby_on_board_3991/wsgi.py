ef2cb9c0174c3f309757d5371fbf57a12f4d0865
Crowdbotics <team@crowdbotics.com>
2019-05-29 21:25:02 +0000

    initial commit

---
+++ backend/baby_on_board_3991/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for baby_on_board_3991 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "baby_on_board_3991.settings")
+
+application = get_wsgi_application()
